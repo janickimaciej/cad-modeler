1694eb4a2df193db5fb6996ab3b1e969e0b7769f
janickimaciej <maciej.m.janicki@gmail.com>
2025-08-28 22:33:36 +0200

    Add small intersection curve textures

--- src/models/intersectable.cpp
+++ src/models/intersectable.cpp
@@ -1,12 +1,5 @@
 #include "models/intersectable.hpp"
 
-#include "framebuffer.hpp"
-
-#include <glad/glad.h>
-
-#include <cstddef>
-#include <stack>
-
 Intersectable::Intersectable(const glm::vec3& pos, const std::string& name,
 	const ChangeCallback& changeCallback, const ShaderProgram& flatShaderProgram) :
 	Model{pos, name},
@@ -34,7 +27,10 @@ void Intersectable::addIntersectionCurve(IntersectionCurve* curve, int surfaceIn
 	m_intersectionCurves.push_back(curve);
 	registerForNotification(curve);
 	m_intersectionCurveTrims.push_back(Trim::none);
-	m_intersectionCurveTextures.push_back(createIntersectionCurveTexture(curve, surfaceIndex));
+	m_intersectionCurveSmallTextures.push_back(
+		createIntersectionCurveTexture<smallTextureSize>(curve, surfaceIndex));
+	m_intersectionCurveBigTextures.push_back(
+		createIntersectionCurveTexture<bigTextureSize>(curve, surfaceIndex));
 }
 
 int Intersectable::intersectionCurveCount() const
@@ -54,9 +50,9 @@ Intersectable::Trim Intersectable::getIntersectionCurveTrim(int index) const
 
 void Intersectable::setIntersectionCurveTrim(int index, Trim trim)
 {
-	for (Trim& trim : m_intersectionCurveTrims)
+	for (Trim& curveTrim : m_intersectionCurveTrims)
 	{
-		trim = Trim::none;
+		curveTrim = Trim::none;
 	}
 	m_trimmingCurve = std::nullopt;
 
@@ -69,7 +65,7 @@ void Intersectable::setIntersectionCurveTrim(int index, Trim trim)
 
 unsigned int Intersectable::getIntersectionCurveTextureId(int index) const
 {
-	return m_intersectionCurveTextures[index].getId();
+	return m_intersectionCurveSmallTextures[index].getId();
 }
 
 void Intersectable::useTrim(const ShaderProgram& surfaceShaderProgram) const
@@ -80,7 +76,7 @@ void Intersectable::useTrim(const ShaderProgram& surfaceShaderProgram) const
 	{
 		surfaceShaderProgram.setUniform("trimSide",
 			static_cast<int>(m_intersectionCurveTrims[*m_trimmingCurve]));
-		m_intersectionCurveTextures[*m_trimmingCurve].use();
+		m_intersectionCurveBigTextures[*m_trimmingCurve].use();
 	}
 }
 
@@ -100,33 +96,6 @@ void Intersectable::registerForNotification(IntersectionCurve* curve)
 		));
 }
 
-Texture Intersectable::createIntersectionCurveTexture(const IntersectionCurve* curve,
-	int surfaceIndex)
-{
-	std::unique_ptr<TextureData> textureData = std::make_unique<TextureData>();
-
-	Framebuffer framebuffer{{textureSize, textureSize}};
-	std::vector<glm::vec2> intersectionPoints = curve->getIntersectionPoints(surfaceIndex);
-	std::unique_ptr<FlatMesh> mesh = createIntersectionMesh(intersectionPoints);
-
-	framebuffer.bind();
-
-	glClearColor(0, 0.5f, 0, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	m_flatShaderProgram.use();
-	mesh->render();
-	framebuffer.getTextureData((*textureData)[0][0].data());
-
-	framebuffer.unbind();
-
-	floodfill(*textureData);
-
-	GLenum uWrap = uWrapped() ? GL_REPEAT : GL_CLAMP_TO_EDGE;
-	GLenum vWrap = vWrapped() ? GL_REPEAT : GL_CLAMP_TO_EDGE;
-	Texture texture{{textureSize, textureSize}, (*textureData)[0][0].data(), uWrap, vWrap};
-	return texture;
-}
-
 void Intersectable::intersectionCurveDestroyNotification(const IntersectionCurve* curve)
 {
 	int curveIndex = getCurveIndex(curve);
@@ -134,7 +103,8 @@ void Intersectable::intersectionCurveDestroyNotification(const IntersectionCurve
 	m_intersectionCurveDestroyNotifications.erase(
 		m_intersectionCurveDestroyNotifications.begin() + curveIndex);
 	m_intersectionCurveTrims.erase(m_intersectionCurveTrims.begin() + curveIndex);
-	m_intersectionCurveTextures.erase(m_intersectionCurveTextures.begin() + curveIndex);
+	m_intersectionCurveSmallTextures.erase(m_intersectionCurveSmallTextures.begin() + curveIndex);
+	m_intersectionCurveBigTextures.erase(m_intersectionCurveBigTextures.begin() + curveIndex);
 	if (m_trimmingCurve.has_value() && *m_trimmingCurve == curveIndex)
 	{
 		m_trimmingCurve = std::nullopt;
@@ -201,93 +171,6 @@ std::unique_ptr<FlatMesh> Intersectable::createIntersectionMesh(
 	}
 
 	return std::make_unique<FlatMesh>(vertices, indices);
-}
-
-void Intersectable::floodfill(TextureData& data) const
-{
-	auto isEmpty =
-		[] (const std::array<unsigned char, 3>& pixel)
-		{
-			return pixel[0] < 128;
-		};
-	auto fill =
-		[] (std::array<unsigned char, 3>& pixel)
-		{
-			pixel[0] = 128;
-			pixel[1] = 0;
-		};
-
-	int startX = 0;
-	int startY = 0;
-	while (!isEmpty(data[startY][startX]))
-	{
-		if (startX == 255)
-		{
-			if (startY == 255)
-			{
-				return;
-			}
-			++startY;
-			startX = 0;
-		}
-		else
-		{
-			++startX;
-		}
-	}
-
-	std::stack<glm::vec2> stack{};
-	fill(data[startY][startX]);
-	stack.push({startX, startY});
-	while (!stack.empty())
-	{
-		glm::vec2 pixel = stack.top();
-		stack.pop();
-
-		if (pixel.x == 0 && uWrapped() && isEmpty(data[pixel.y][textureSize - 1]))
-		{
-			fill(data[pixel.y][textureSize - 1]);
-			stack.push({textureSize - 1, pixel.y});
-		}
-		if (pixel.x != 0 && isEmpty(data[pixel.y][pixel.x - 1]))
-		{
-			fill(data[pixel.y][pixel.x - 1]);
-			stack.push({pixel.x - 1, pixel.y});
-		}
-
-		if (pixel.x == textureSize - 1 && uWrapped() && isEmpty(data[pixel.y][0]))
-		{
-			fill(data[pixel.y][0]);
-			stack.push({0, pixel.y});
-		}
-		if (pixel.x != textureSize - 1 && isEmpty(data[pixel.y][pixel.x + 1]))
-		{
-			fill(data[pixel.y][pixel.x + 1]);
-			stack.push({pixel.x + 1, pixel.y});
-		}
-
-		if (pixel.y == 0 && vWrapped() && isEmpty(data[textureSize - 1][pixel.x]))
-		{
-			fill(data[textureSize - 1][pixel.x]);
-			stack.push({pixel.x, textureSize - 1});
-		}
-		if (pixel.y != 0 && isEmpty(data[pixel.y - 1][pixel.x]))
-		{
-			fill(data[pixel.y - 1][pixel.x]);
-			stack.push({pixel.x, pixel.y - 1});
-		}
-
-		if (pixel.y == textureSize - 1 && vWrapped() && isEmpty(data[0][pixel.x]))
-		{
-			fill(data[0][pixel.x]);
-			stack.push({pixel.x, 0});
-		}
-		if (pixel.y != textureSize - 1 && isEmpty(data[pixel.y + 1][pixel.x]))
-		{
-			fill(data[pixel.y + 1][pixel.x]);
-			stack.push({pixel.x, pixel.y + 1});
-		}
-	}
 }
 
 int Intersectable::getCurveIndex(const IntersectionCurve* curve) const
--- src/models/intersectable.hpp
+++ src/models/intersectable.hpp
@@ -1,24 +1,30 @@
 #pragma once
 
+#include "framebuffer.hpp"
 #include "meshes/flatMesh.hpp"
 #include "models/intersectionCurve.hpp"
 #include "models/model.hpp"
 #include "shaderProgram.hpp"
 #include "texture.hpp"
 
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 
 #include <array>
 #include <functional>
 #include <memory>
+#include <stack>
 #include <string>
 #include <vector>
 
 class Intersectable : public Model
 {
-	static constexpr int textureSize = 1024;
-	using TextureData =
-		std::array<std::array<std::array<unsigned char, 3>, textureSize>, textureSize>;
+	static constexpr int smallTextureSize = 256;
+	static constexpr int bigTextureSize = 1024;
+
+	template <int size>
+	using TextureData = std::array<std::array<std::array<unsigned char, 3>, size>, size>;
+
 public:
 	enum class Trim
 	{
@@ -60,18 +66,140 @@ private:
 	std::vector<std::shared_ptr<IntersectionCurve::DestroyCallback>>
 		m_intersectionCurveDestroyNotifications{};
 	std::vector<Trim> m_intersectionCurveTrims{};
-	std::vector<Texture> m_intersectionCurveTextures{};
+	std::vector<Texture> m_intersectionCurveSmallTextures{};
+	std::vector<Texture> m_intersectionCurveBigTextures{};
 	std::optional<int> m_trimmingCurve = std::nullopt;
 	const ShaderProgram& m_flatShaderProgram;
 
 	ChangeCallback m_changeCallback;
 
 	void registerForNotification(IntersectionCurve* curve);
-	Texture createIntersectionCurveTexture(const IntersectionCurve* curve, int surfaceIndex);
 	void intersectionCurveDestroyNotification(const IntersectionCurve* curve);
 	std::unique_ptr<FlatMesh> createIntersectionMesh(
 		const std::vector<glm::vec2>& intersectionPoints) const;
-	void floodfill(TextureData& data) const;
 	int getCurveIndex(const IntersectionCurve* curve) const;
 	static glm::vec2 params2Tex(const glm::vec2& parameters);
+
+	template <int textureSize>
+	Texture createIntersectionCurveTexture(const IntersectionCurve* curve, int surfaceIndex);
+
+	template <int textureSize>
+	void floodfill(TextureData<textureSize>& data) const;
 };
+
+template <int textureSize>
+Texture Intersectable::createIntersectionCurveTexture(const IntersectionCurve* curve,
+	int surfaceIndex)
+{
+	std::unique_ptr<TextureData<textureSize>> textureData =
+		std::make_unique<TextureData<textureSize>>();
+
+	Framebuffer framebuffer{{textureSize, textureSize}};
+	std::vector<glm::vec2> intersectionPoints = curve->getIntersectionPoints(surfaceIndex);
+	std::unique_ptr<FlatMesh> mesh = createIntersectionMesh(intersectionPoints);
+
+	framebuffer.bind();
+
+	glClearColor(0, 0.5f, 0, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	m_flatShaderProgram.use();
+	mesh->render();
+	framebuffer.getTextureData((*textureData)[0][0].data());
+
+	framebuffer.unbind();
+
+	floodfill<textureSize>(*textureData);
+
+	GLenum uWrap = uWrapped() ? GL_REPEAT : GL_CLAMP_TO_EDGE;
+	GLenum vWrap = vWrapped() ? GL_REPEAT : GL_CLAMP_TO_EDGE;
+	Texture texture{{textureSize, textureSize}, (*textureData)[0][0].data(), uWrap, vWrap};
+	return texture;
+}
+
+template <int textureSize>
+void Intersectable::floodfill(TextureData<textureSize>& data) const
+{
+	auto isEmpty =
+		[] (const std::array<unsigned char, 3>& pixel)
+		{
+			return pixel[0] < 128;
+		};
+	auto fill =
+		[] (std::array<unsigned char, 3>& pixel)
+		{
+			pixel[0] = 128;
+			pixel[1] = 0;
+		};
+
+	int startX = 0;
+	int startY = 0;
+	while (!isEmpty(data[startY][startX]))
+	{
+		if (startX == 255)
+		{
+			if (startY == 255)
+			{
+				return;
+			}
+			++startY;
+			startX = 0;
+		}
+		else
+		{
+			++startX;
+		}
+	}
+
+	std::stack<glm::vec2> stack{};
+	fill(data[startY][startX]);
+	stack.push({startX, startY});
+	while (!stack.empty())
+	{
+		glm::vec2 pixel = stack.top();
+		stack.pop();
+
+		if (pixel.x == 0 && uWrapped() && isEmpty(data[pixel.y][textureSize - 1]))
+		{
+			fill(data[pixel.y][textureSize - 1]);
+			stack.push({textureSize - 1, pixel.y});
+		}
+		if (pixel.x != 0 && isEmpty(data[pixel.y][pixel.x - 1]))
+		{
+			fill(data[pixel.y][pixel.x - 1]);
+			stack.push({pixel.x - 1, pixel.y});
+		}
+
+		if (pixel.x == textureSize - 1 && uWrapped() && isEmpty(data[pixel.y][0]))
+		{
+			fill(data[pixel.y][0]);
+			stack.push({0, pixel.y});
+		}
+		if (pixel.x != textureSize - 1 && isEmpty(data[pixel.y][pixel.x + 1]))
+		{
+			fill(data[pixel.y][pixel.x + 1]);
+			stack.push({pixel.x + 1, pixel.y});
+		}
+
+		if (pixel.y == 0 && vWrapped() && isEmpty(data[textureSize - 1][pixel.x]))
+		{
+			fill(data[textureSize - 1][pixel.x]);
+			stack.push({pixel.x, textureSize - 1});
+		}
+		if (pixel.y != 0 && isEmpty(data[pixel.y - 1][pixel.x]))
+		{
+			fill(data[pixel.y - 1][pixel.x]);
+			stack.push({pixel.x, pixel.y - 1});
+		}
+
+		if (pixel.y == textureSize - 1 && vWrapped() && isEmpty(data[0][pixel.x]))
+		{
+			fill(data[0][pixel.x]);
+			stack.push({pixel.x, 0});
+		}
+		if (pixel.y != textureSize - 1 && isEmpty(data[pixel.y + 1][pixel.x]))
+		{
+			fill(data[pixel.y + 1][pixel.x]);
+			stack.push({pixel.x, pixel.y + 1});
+		}
+	}
+}
